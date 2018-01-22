5b4fc87a93f14c813f831f885f4b922cf3f6d197
Carlos Kazuhira <Carlos Kazuhira@DESKTOP-NSS3E2M>
2018-01-22 17:29:27 +0100

    Agregar archivos de proyecto.

---
+++ Prueba_GIT_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prueba_GIT_MVC.MvcApplication" Language="C#" %>
---
+++ Prueba_GIT_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prueba_GIT_MVC.Startup))]
+namespace Prueba_GIT_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
